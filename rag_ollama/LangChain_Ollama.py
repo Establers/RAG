6f7d0448b307729dd5dfb88fc1e584d3ecc30a8b
Establers <44702967+Establers@users.noreply.github.com>
2024-07-19 06:49:19 +0900

    ollama

--- rag_ollama/LangChain_Ollama.py
+++ rag_ollama/LangChain_Ollama.py
@@ -1,23 +1,10 @@
-from dotenv import load_dotenv
-import os
-
-import sys
-import io
-# 표준 출력의 인코딩을 UTF-8로 설정
-sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
-sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
-
-load_dotenv()
-
-print(f"[API KEY]\n{os.environ['OPENAI_API_KEY']}")
-
-# LangSmith 추적을 설정합니다. https://smith.langchain.com
+# !pip install langchain-teddynote
 from langchain_teddynote import logging
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_teddynote.messages import stream_response
 
-# 프로젝트 이름을 입력합니다.
-logging.langsmith("CH12-Basic")
-
-from langchain_openai import ChatOpenAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_community.vectorstores import FAISS
@@ -26,14 +13,18 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
-# 객체 생성
-# llm = ChatOpenAI(
-#     temperature=0.1,  # 창의성 (0.0 ~ 2.0)
-#     model_name="gpt-4o",  # 모델명
-# )
+#### 표준 출력의 인코딩을 UTF-8로 설정
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
+#### 
 
-
-from langchain_teddynote.models import MultiModal
+from dotenv import load_dotenv
+import os
+load_dotenv()
+# 프로젝트 이름을 입력합니다.
+logging.langsmith("LangChain_Ollama")
 
 # 단계 1: 문서 로드(Load Documents)
 loader = PyMuPDFLoader("docs/REN_r01uh0495ej0100_rx634_MAH_20150225.pdf")
@@ -46,7 +37,17 @@ split_documents = text_splitter.split_documents(docs)
 print(f"분할된 청크의수: {len(split_documents)}")
 
 # 단계 3: 임베딩(Embedding) 생성
-embeddings = OpenAIEmbeddings()
+# embeddings = OpenAIEmbeddings()
+
+# 허깅 페이스 로컬 임베딩 사용
+from langchain_community.embeddings import HuggingFaceHubEmbeddings
+embeddings = HuggingFaceHubEmbeddings()
+text = (
+    "임베딩 테스트를 하기 위한 샘플 문장입니다."  # 테스트용 문서 텍스트를 정의합니다.
+)
+query_result = embeddings.embed_query(text)
+len(query_result)
+query_result[:3]
 
 # 단계 4: DB 생성(Create DB) 및 저장
 # 벡터스토어를 생성합니다.
@@ -59,7 +60,8 @@ retriever = vectorstore.as_retriever()
 # 검색기에 쿼리를 날려 검색된 chunk 결과를 확인합니다.
 retriever.invoke("SCI 통신 레지스터를 설정하려고 하는데 주의해야할 점은 뭐야? 자세하게 알려줘")
 
-print(retriever)
+# Ollama 모델을 불러옵니다.
+llm = ChatOllama(model="EEVE-Korean-10.8B:latest")
 
 prompt = PromptTemplate.from_template(
     """You are an assistant for question-answering tasks. 
@@ -76,15 +78,7 @@ prompt = PromptTemplate.from_template(
     #Answer:"""
 )
 
-# 단계 7: 언어모델(LLM) 생성
-# 모델(LLM) 을 생성합니다.
-llm = ChatOpenAI(model_name="gpt-4o", temperature=0.1)
-"""_summary_
-temperature 가 낮을 수록 보수적이고, 높을 수록 창의적인 답변을 생성.
-"""
-
-# LCEL LangChain Expression Language
-# 단계 8: 체인(Chain) 생성 (뭔가 바껴야할 때 다시 실행해야함)
+# 체인 생성
 chain = (
     {"context": retriever, "question": RunnablePassthrough()}   # 프롬프트에 들어가는 변수에 해당하는 값
     | prompt
@@ -92,9 +86,8 @@ chain = (
     | StrOutputParser() # 답변을 할 때 사용하는 출력 파서, 최종 문자열로 나온다?
 )
 
-# 체인 실행(Run Chain)
-# 문서에 대한 질의를 입력하고, 답변을 출력합니다."
-question = "SCI 통신 레지스터를 설정하려고 하는데 주의해야할 점은 뭐야? 자세하게 알려줘"# 위에서 정의한 question에 들어갈 내용
-# invoke 함수에서 question을 입력하면 체인이 실행되어 답변을 출력합니다.
-response = chain.invoke(question)
-print(response)
+# 간결성을 위해 응답은 터미널에 출력됩니다.
+answer = chain.stream("SCI 통신 레지스터를 설정하려고 하는데 주의해야할 점은 뭐야? 자세하게 알려줘")
+
+# 스트리밍 출력
+stream_response(answer)
---
+++ rag_ollama/README.md
@@ -0,0 +1,29 @@
+## Model Download
+https://huggingface.co/teddylee777/EEVE-Korean-Instruct-10.8B-v1.0-gguf/tree/main
+- 집에서 `EEVE-Korean-Instruct-10.8B-v1.0-Q5_K_M.gguf` 로 모델 다운 받았음.
+
+### Modelfile 설정
+Modelfile 해당 내용 파일 생성 후, 복사 붙여넣기 
+
+### ollama create
+```
+ollama create EEVE-Korean-10.8B -f Modelfile
+// Modelfile 이 있는 폴더에서 수행
+```
+
+### ollama list
+```
+ollama list
+```
+```
+NAME                    ID              SIZE    MODIFIED      
+EEVE-Korean-10.8B:latest        4d957747ec33    7.7 GB  5 seconds ago  
+```
+- 모델이 추가 된 것을 확인할 수 있다.
+
+### HuggingFace Open Embedding model Download
+```
+git lfs clone https://huggingface.co/intfloat/multilingual-e5-large-instruct
+```
+
+
